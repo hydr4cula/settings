048391420392acd4a769a84c856e24b17f0d964e
jaustinwhite21 <j.austinwhite21@gmail.com>
2016-10-05 17:20:23 -0500

    first

---
+++ src/rainmet/LoginController.java
@@ -0,0 +1,56 @@
+package rainmet;
+
+import java.awt.Color;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * Login Controller.
+ */
+public class LoginController extends AnchorPane implements Initializable {
+
+    @FXML
+    TextField userId;
+    @FXML
+    PasswordField password;
+    @FXML
+    Button login;
+    @FXML
+    Label errorMessage;
+
+    private Main application;
+
+
+    public void setApp(Main application){
+        this.application = application;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        errorMessage.setText("");
+        userId.setPromptText("username");
+        password.setPromptText("password");
+
+    }
+
+
+    public void processLogin(ActionEvent event) {
+        if (application == null){
+            // We are running in isolated FXML, possibly in Scene Builder.
+            // NO-OP.
+            errorMessage.setText("Hello " + userId.getText());
+        } else {
+            if (!application.userLogging(userId.getText(), password.getText())){
+                errorMessage.setText("Username/Password is incorrect");
+            }
+        }
+    }
+}
---
+++ src/rainmet/security/Authenticator.java
@@ -0,0 +1,19 @@
+package rainmet.security;
+
+/**
+ * Created by austinwhite on 10/4/16.
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Authenticator {
+    private static final Map<String, String> USERS = new HashMap<String, String>();
+    static {
+        USERS.put("demo", "demo");
+    }
+    public static boolean validate(String user, String password){
+        String validUserPassword = USERS.get(user);
+        return validUserPassword != null && validUserPassword.equals(password);
+    }
+}
